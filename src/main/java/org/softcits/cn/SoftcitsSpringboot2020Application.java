85c2506e909f5b84615dbd7643693ca160b48978
Jun <wangzhen_tju@126.com>
2020-12-05 15:28:40 +0800

    init

---
+++ src/main/java/org/softcits/cn/SoftcitsSpringboot2020Application.java
@@ -0,0 +1,13 @@
+package org.softcits.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftcitsSpringboot2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftcitsSpringboot2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/softcits/cn/SoftcitsSpringboot2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softcits.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftcitsSpringboot2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
