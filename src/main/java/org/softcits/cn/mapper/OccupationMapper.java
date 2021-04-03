4573381ba74144a838dc4fa6b5ebf3adcbcd6a7c
Jun <wangzhen_tju@126.com>
2021-04-03 11:16:39 +0800

    集合查询

--- src/main/java/org/softcits/cn/mapper/OccupationMapper.java
+++ src/main/java/org/softcits/cn/mapper/OccupationMapper.java
@@ -1,10 +1,14 @@
 package org.softcits.cn.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.softcits.cn.model.Occupation;
 @Mapper
 public interface OccupationMapper {
 
 	Occupation getFirstOccupation();
+	
+	List<Occupation> getAllOccupations();
 	
 }
--- src/test/java/org/softcits/cn/serivce/OccupationServiceTest.java
+++ src/test/java/org/softcits/cn/serivce/OccupationServiceTest.java
@@ -1,7 +1,10 @@
 package org.softcits.cn.serivce;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.softcits.cn.mapper.OccupationMapper;
 import org.softcits.cn.model.Occupation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +14,8 @@ public class OccupationServiceTest {
 	
 	@Autowired
 	private OccupationService occupationService;
+	@Autowired
+	private OccupationMapper occupationMapper;
 
 	@Test
 	public void getFirstOccupationTest() {
@@ -18,4 +23,17 @@ public class OccupationServiceTest {
 		
 		Assertions.assertNotNull(op);
 	}
+	
+	@Test
+	public void getAllOccupationsTest() {
+		List<Occupation> occList = occupationMapper.getAllOccupations();
+		Assertions.assertNotEquals(0, occList.size());
+		System.out.println("Total Size: " + occList.size());
+	}
 }
+
+
+
+
+
+
