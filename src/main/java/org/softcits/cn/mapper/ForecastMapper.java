221ab212903577b4af97f2f5ae0cd3da3abb3e0e
Jun <wangzhen_tju@126.com>
2021-04-10 13:14:32 +0800

    Add @Mapper

--- src/main/java/org/softcits/cn/mapper/ForecastMapper.java
+++ src/main/java/org/softcits/cn/mapper/ForecastMapper.java
@@ -2,9 +2,10 @@ package org.softcits.cn.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.softcits.cn.model.Forecast;
-
+@Mapper
 public interface ForecastMapper {
 	
 	List<Forecast> getForecastByCityId(@Param(value="cid")String cid);
