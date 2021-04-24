df743ca6d32ed3cf243d3e2996f92eb04aa94139
Jun <wangzhen_tju@126.com>
2021-04-24 14:04:10 +0800

    @Transanctional

--- src/main/java/org/softcits/cn/serivce/CityForecastServiceImpl.java
+++ src/main/java/org/softcits/cn/serivce/CityForecastServiceImpl.java
@@ -4,12 +4,15 @@ import org.softcits.cn.mapper.CityForecastMapper;
 import org.softcits.cn.model.CityForecast;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CityForecastServiceImpl implements CityForecastService {
 
 	@Autowired
 	private CityForecastMapper cityForecastMapper;
 	
+	@Transactional(isolation = Isolation.REPEATABLE_READ)
 	@Override
 	public CityForecast getForcastWithCityByCityId(String cid) {
 		CityForecast cityForecast = cityForecastMapper.getForcastWithCityByCityId(cid);
