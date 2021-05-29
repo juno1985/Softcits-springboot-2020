5379bbbcebfe85cdf56cb25bb9396922ccc0e140
Jun <wangzhen_tju@126.com>
2021-05-29 14:50:15 +0800

    Notice数据插入

--- src/main/java/org/softcits/cn/mapper/CityNoticeMapper.java
+++ src/main/java/org/softcits/cn/mapper/CityNoticeMapper.java
@@ -3,9 +3,10 @@ package org.softcits.cn.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.softcits.cn.model.CityNotice;
+import org.softcits.cn.model.Notice;
 @Mapper
 public interface CityNoticeMapper {
 
 	CityNotice getCityNoticeByCityId(@Param(value="city_id") String city_id);
-	
+	Integer insert(Notice notice);
 }
--- src/main/java/org/softcits/cn/serivce/RemoteDataServiceImpl.java
+++ src/main/java/org/softcits/cn/serivce/RemoteDataServiceImpl.java
@@ -3,9 +3,12 @@ package org.softcits.cn.serivce;
 import java.util.List;
 
 import org.softcits.cn.mapper.CityMapper;
+import org.softcits.cn.mapper.CityNoticeMapper;
 import org.softcits.cn.mapper.YesterdayMapper;
 import org.softcits.cn.model.City;
+import org.softcits.cn.model.Notice;
 import org.softcits.cn.model.Yesterday;
+import org.softcits.cn.pojo.Data;
 import org.softcits.cn.pojo.ForecastPojo;
 import org.softcits.cn.pojo.Response;
 import org.softcits.cn.pojo.YesterdayPojo;
@@ -13,6 +16,9 @@ import org.softcits.cn.util.JSONObjectConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -29,6 +35,8 @@ public class RemoteDataServiceImpl implements RemoteDataService {
 	private CityMapper cityMapper;
 	@Autowired
 	private YesterdayMapper yesterdayMapper;
+	@Autowired
+	private CityNoticeMapper cityNoticeMapper; 
 	@Override
 	public String getRemoteData(String url) {
 		
@@ -46,6 +54,7 @@ public class RemoteDataServiceImpl implements RemoteDataService {
 		};
 		return JSONObjectConverter.generateObjectFromJSON(jsonStr, typeReference);
 	}
+	@Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRED)
 	@Override
 	public void initSingleWeatherData(String cityId) {
 		
@@ -54,13 +63,14 @@ public class RemoteDataServiceImpl implements RemoteDataService {
 		String json = this.getRemoteData(url);
 		// convert json to Response object
 		Response response = this.getResponseFromJSON(json);
+		Data data = response.getData();
 		// insert forecast data into mysql
-		List<ForecastPojo> forecastPojoList = response.getData().getForecast(); 
+		List<ForecastPojo> forecastPojoList = data.getForecast(); 
 		City city = cityMapper.getCityByCityId(cityId);
 		Integer cid = city.getId();
 		forecastService.insert(forecastPojoList, cid);
 		// insert yesterday data into mysql
-		YesterdayPojo yesterdayPojo = response.getData().getYesterday();
+		YesterdayPojo yesterdayPojo = data.getYesterday();
 		Yesterday yesterday = new Yesterday();
 		yesterday.setCid(cid);
 		yesterday.setDate(yesterdayPojo.getDate());
@@ -70,6 +80,12 @@ public class RemoteDataServiceImpl implements RemoteDataService {
 		yesterday.setLow(yesterdayPojo.getLow());
 		yesterday.setType(yesterdayPojo.getType());
 		yesterdayMapper.insert(yesterday);
+		// insert notice data into mysql
+		Notice notice = new Notice();
+		notice.setGanmao(data.getGanmao());
+		notice.setWendu(data.getWendu());
+		notice.setCid(cid);
+		cityNoticeMapper.insert(notice);
 	}
 	@Override
 	public void initBatchWeatherData() {
